4e5a5206d6d0ff71065b35e4a1b9a3d1899c2124
LF56 <leah.fox7@gmail.com>
2022-01-31 14:10:04 -0500

    submit button functoning in JS

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+var cityInputEl = document.querySelector("city");
+
+
+
+
+
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+}
+//get input value
+var city = cityInputEl;
+ 
+
+//unique API key to get weather information
+var APIKey = "00d7248bf8115d5f89fd7b582d330c1f";
+
