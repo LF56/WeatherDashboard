ec66c3ed534f758ee1d0279b72ce2926b4abfa46
LF56 <leah.fox7@gmail.com>
2022-01-31 20:39:49 -0500

    added api link

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-var cityInputEl = document.querySelector("city");
+//unique API key to get weather information
+var APIKey = "00d7248bf8115d5f89fd7b582d330c1f";
+var city= ""
+
+fetch("http://api.openweathermap.org/geo/1.0/direct?q=orlando&limit=1&appid=" + APIKey ).then(response => 
+    response.json()
+).then(data =>
+    console.log(data)
+)
 
 
 
-
-
-
+//submit input button
 var formSubmitHandler = function(event){
     event.preventDefault();
 }
-//get input value
-var city = cityInputEl;
- 
-
-//unique API key to get weather information
-var APIKey = "00d7248bf8115d5f89fd7b582d330c1f";
 
